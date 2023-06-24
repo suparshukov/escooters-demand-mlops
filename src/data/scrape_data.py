65e6a3b2a687748fe46e78247293108c6f192af2
Sergey Parshukov <sparshukov85@gmail.com>
2023-06-24 14:05:11 +0200

    Load external data - the coordinates of the city's center

--- src/data/scrape_data.py
+++ src/data/scrape_data.py
@@ -1,31 +1,65 @@
-# -*- coding: utf-8 -*-
+"""Module providing functions for scraping external data"""
 import logging
 from pathlib import Path
+from typing import Tuple
 
 import click
-from dotenv import find_dotenv, load_dotenv
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_city_center_coordinates(
+    url: str, city_center_filepath: Path
+) -> Tuple[float, float]:
+    """
+    Scrapes data on city center coordinates and saves it to a text file
+    @param url: str, the address of the page to scrape
+    the coordinates of the city center from
+    @param city_center_filepath: Path, the path of a file
+    to save the city center coordinates into
+    @return: Tuple[float, float], the coordinates of the city center
+    """
+
+    if city_center_filepath.exists():
+        with open(city_center_filepath, "r", encoding="utf-8") as file_with_coords:
+            coordinates_str_list = file_with_coords.read().split(",")
+            lat, lon = float(coordinates_str_list[0]), float(coordinates_str_list[1])
+            print("skipping scrapping")
+    else:
+        html_doc = requests.get(url, allow_redirects=True, timeout=3).content
+        soup = BeautifulSoup(html_doc, "html.parser")
+        coordinates_element = soup.find_all(
+            lambda tag: len(tag.find_all("p")) == 0
+            and "Latitude and longitude coordinates are:" in tag.text
+        )[0]
+        coordinates_str = coordinates_element.find("strong").text
+        city_center_filepath.parent.mkdir(parents=True, exist_ok=True)
+        with open(
+            city_center_filepath, "w", encoding="utf-8"
+        ) as file_with_coords:
+            file_with_coords.write(coordinates_str)
+        print(coordinates_str)
+        coordinates_str_list = coordinates_str.split(",")
+        lat, lon = float(coordinates_str_list[0]), float(coordinates_str_list[1])
+        print("scrapping finished")
+
+    return lat, lon
 
 
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+@click.option("--coordinates_page_url", default="https://www.latlong.net/place/chicago-il-usa-1855.html", help="Url of a page with the city center coordinates")
+@click.option("--path_to_external_data", default="./data/external", help="Path to external data")
+def main(coordinates_page_url: str, path_to_external_data: str):
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
+    city_center_coordinates_filepath = Path.joinpath(
+        Path(path_to_external_data), "city_center_coordinates.txt"
+    )
+    scrape_city_center_coordinates(
+        coordinates_page_url, city_center_coordinates_filepath
+    )
 
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
+if __name__ == "__main__":
     main()
